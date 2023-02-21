01811789581010f8dbeb6be244663b1af189dfb2
infoengine1337 <103350279+infoengine1337@users.noreply.github.com>
2023-02-21 12:30:09 +0900

    [update] create crud user module & enable swagger

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,29 @@ declare const module: any;
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+        whitelist: true,
+        transform: true,
+        disableErrorMessages: true,
+    }),
+  );
+  
+  const config = new DocumentBuilder()
+  .setTitle('EkanProton Backend API')
+  .setDescription('The EkanProton Backend API description')
+  .setVersion('0.0.1')
+  .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, document);
+  
   await app.listen(3001);
 
   if (module.hot) {
